d46a4a9145b017cd8c335ecec3fdc9fbbb618cf9
ellysaschottler <ellysastork@gmail.com>
2024-03-18 12:05:15 -0500

    delete current contact function finished

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,7 +36,8 @@ const ContactState = props => {
                 phone: '333-333-3333',
                 type: 'professional'
             }
-        ]
+        ],
+        current: null
     }
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -52,9 +53,13 @@ const ContactState = props => {
         dispatch({ type: DELETE_CONTACT, payload: id })
     }
     // Set Current Contact
-
+    const setCurrent = contact => {
+        dispatch({ type:SET_CURRENT, payload: contact })
+    }
     // Clear Current Contact
-
+    const clearCurrent = () => {
+        dispatch({ type:CLEAR_CURRENT})
+    }
     // Update Contact
 
     // Filter Contacts
@@ -65,8 +70,11 @@ const ContactState = props => {
     return (
         <ContactContext.Provider value={{
             contacts: state.contacts,
+            current: state.current,
             addContact,
-            deleteContact
+            deleteContact,
+            setCurrent,
+            clearCurrent
         }}>
             {props.children}
         </ContactContext.Provider>
