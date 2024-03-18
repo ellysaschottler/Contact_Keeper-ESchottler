e42ae606f83a3e02fc88b1a55bbfea5fedab15f4
ellysaschottler <ellysastork@gmail.com>
2024-03-18 18:25:39 -0500

    created register and login components

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,12 +12,13 @@ const Navbar = ({ title, icon }) => {
                 <li>
                     <Link to='/'>Home</Link>
                     <Link to='/about'>About</Link>
+                    <Link to='/register'>Register</Link>
+                    <Link to='/login'>Login</Link>
                 </li>
             </ul>
         </div>
     )
 }
-
 
 Navbar.propTypes = {
     title: PropTypes.string.isRequired,
