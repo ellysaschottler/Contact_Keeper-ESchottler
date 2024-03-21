b3a8464dda5846471945b5ab4d83a6f29e9d92bc
ellysaschottler <ellysastork@gmail.com>
2024-03-21 16:09:02 -0500

    get and clear contacts completed

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,4 +1,5 @@
 import {
+    GET_CONTACTS,
     ADD_CONTACT,
     DELETE_CONTACT,
     SET_CURRENT,
@@ -6,20 +7,37 @@ import {
     UPDATE_CONTACT,
     FILTER_CONTACTS,
     CLEAR_FILTER,
-    CONTACT_ERROR
+    CONTACT_ERROR,
+    CLEAR_CONTACTS
 } from '../types'
 
 const contactReducer = (state, action) => {
     switch(action.type) {
+        case GET_CONTACTS:
+            return {
+                ...state,
+                contacts: action.payload,
+                loading: false
+            }
         case ADD_CONTACT:
             return {
                 ...state,
-                contacts: [...state.contacts, action.payload]
+                contacts: [...state.contacts, action.payload],
+                loading: false
             }
         case DELETE_CONTACT:
             return {
                 ...state,
-                contacts: state.contacts.filter(contact => contact.id !== action.payload)
+                contacts: state.contacts.filter(contact => contact.id !== action.payload),
+                loading: false
+        }
+        case CLEAR_CONTACTS:
+            return {
+                ...state,
+                contacts: null,
+                filtered: null,
+                error: null,
+                current: null
         }
         case SET_CURRENT:
              return {
@@ -34,7 +52,8 @@ const contactReducer = (state, action) => {
         case UPDATE_CONTACT:
             return {
                     ...state,
-                contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload: contact)
+                contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload: contact),
+                loading: false
                 }
 
         case FILTER_CONTACTS:
