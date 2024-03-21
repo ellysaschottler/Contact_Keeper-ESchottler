7e5e6de622d97af892016a9f326bdff8ec8a7d1a
ellysaschottler <ellysastork@gmail.com>
2024-03-21 13:35:22 -0500

    made private route to apply to home page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
+import PrivateRoute from './components/routing/PrivateRoute';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
@@ -29,7 +30,7 @@ const App = () => {
       <div className="container">
         <Alerts />
         <Routes>
-          <Route exact path='/' element={<Home/>} />
+          <PrivateRoute exact path='/' element={<Home/>} />
           <Route exact path='about' element={<About/>} />
           <Route exact path='register' element={<Register/>} />
           <Route exact path='login' element={<Login/>} />
